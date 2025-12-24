bcfce09569ec942544e48a31a1abd320ed2d6224
Luigi Linari <luigi.linari@studio.unibo.it>
2025-12-24 19:53:28 +0100

    up to date database added

--- db/database.sql
+++ db/database.sql
@@ -1,41 +1,60 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Database Section
+-- ________________ 
 
--- docker compose down -v 
--- comando per eliminare i volumi e eseguire sta roba al prox compose up
---
--- documentazione mysql per blob/text (mi parevano piu adatti di varchar)
--- https://dev.mysql.com/doc/refman/9.5/en/blob.html
+create database if not exists unibostu;
+use unibostu;
+
 
 -- Tables Section
 -- _____________ 
+
+create table amministratori (
+     idamministratore varchar(60) not null,
+     identita int not null,
+     password varchar(255) not null,
+     constraint IDamministratori primary key (idamministratore),
+     constraint FKR_ID unique (identita));
+
+create table castegorie_posts (
+     idpost int not null,
+     idcategoria varchar(30) not null,
+     constraint IDcastegorie_post primary key (idcategoria, idpost));
+
+create table categorie (
+     idcategoria varchar(30) not null,
+     riservata char not null,
+     constraint IDcategorie primary key (idcategoria));
+
+create table commenti (
+     idpost int not null,
+     idcommento int not null,
+     testo text not null,
+     data_creazione date not null,
+     identita int not null,
+     idpost_genitore int not null,
+     idcommento_genitore int not null,
+     constraint IDcommenti primary key (idpost, idcommento));
+
+create table corsi (
+     idcorso int not null auto_increment,
+     nome_corso varchar(60) not null,
+     idfacolta int not null,
+     constraint IDcorso primary key (idcorso));
+
+create table entita (
+     identita int not null auto_increment,
+     constraint IDcf primary key (identita));
 
 create table facolta (
      idfacolta int not null auto_increment,
      nome_facolta varchar(60) not null,
      constraint IDcorsi_di_laurea primary key (idfacolta));
 
-create table utenti (
-     username varchar(60) not null,
-     password varchar(255) not null,
-     nome varchar(30) not null,
-     cognome varchar(30) not null,
-     idfacolta int not null,
-     constraint IDutenti primary key (username));
-
-create table amministratori (
-     username varchar(30) not null,
-     password varchar(255) not null,
-     constraint IDamministratori primary key (username));
-
-create table corsi (
-     idcorso int not null auto_increment,
-     nome_corso varchar(60) not null,
-     idfacolta int,
-     constraint IDcorso primary key (idcorso));
-
-insert into corsi (idcorso, nome_corso) values (0, "tag globali");
+create table post_tags (
+     idpost int not null,
+     idcorso int not null,
+     tipo varchar(30) not null,
+     constraint IDpost_tags primary key (idpost, idcorso, tipo));
 
 create table posts (
      idpost int not null auto_increment,
@@ -44,66 +63,99 @@ create table posts (
      allegato blob null,
      likes int not null default 0,
      dislikes int not null default 0,
-     idutente varchar(60) not null,
+     data_creazione date not null,
+     identita int not null,
+     idcorso int,
      constraint IDposts primary key (idpost));
 
 create table tags (
-     tipo varchar(30) not null,     -- Tipo del tag (prima parte della PK)
-     idcorso int not null,          -- ID del corso associato (seconda parte della PK)
-     constraint IDtags primary key (tipo, idcorso));
+     tipo varchar(30) not null,
+     idcorso int not null,
+     constraint IDtags primary key (idcorso, tipo));
 
-create table tags_post (
-     tipo varchar(30) not null,     -- Parte della FK a tags.tipo
-     idcorso int not null,          -- Parte della FK a tags.idcorso  
-     idpost int not null,           -- FK a posts.idpost
-     constraint IDtags_post primary key (tipo, idcorso, idpost));
-
-create table commenti (
-     idcommento int not null auto_increment,
-     idpost int not null,
-     testo text not null,
+create table utenti (
      idutente varchar(60) not null,
-     commento_genitore int null,
-     constraint IDcommenti primary key (idcommento));
+     identita int not null,
+     password varchar(255) not null,
+     nome varchar(30) not null,
+     cognome varchar(30) not null,
+     idfacolta int not null,
+     constraint IDutenti primary key (idutente),
+     constraint FKR_1_ID unique (identita));
+
+create table visione_posts (
+     idpost int not null,
+     idfacolta int not null,
+     constraint IDvisione_post primary key (idfacolta, idpost));
 
 
 -- Constraints Section
 -- ___________________ 
 
+alter table amministratori add constraint FKR_FK
+     foreign key (identita)
+     references entita (identita);
+
+alter table castegorie_posts add constraint FKcas_cat
+     foreign key (idcategoria)
+     references categorie (idcategoria);
+
+alter table castegorie_posts add constraint FKcas_pos
+     foreign key (idpost)
+     references posts (idpost);
+
 alter table commenti add constraint FKscrittura
-     foreign key (idutente)
-     references utenti (username);
+     foreign key (identita)
+     references entita (identita);
 
 alter table commenti add constraint FKcommenti_posts
      foreign key (idpost)
      references posts (idpost);
 
-alter table commenti add constraint FKcommenti_genitore
-     foreign key (commento_genitore)
-     references commenti (idcommento);
+alter table commenti add constraint FKrisposte
+     foreign key (idpost_genitore, idcommento_genitore)
+     references commenti (idpost, idcommento);
 
 alter table corsi add constraint FKcomposizione
      foreign key (idfacolta)
      references facolta (idfacolta);
 
+alter table post_tags add constraint FKpos_tag
+     foreign key (idcorso, tipo)
+     references tags (idcorso, tipo);
+
+alter table post_tags add constraint FKpos_pos
+     foreign key (idpost)
+     references posts (idpost);
+
 alter table posts add constraint FKcreazione
-     foreign key (idutente)
-     references utenti (username);
+     foreign key (identita)
+     references entita (identita);
+
+alter table posts add constraint FKcorso_posts
+     foreign key (idcorso)
+     references corsi (idcorso);
 
 alter table tags add constraint FKcorsi_tags
      foreign key (idcorso)
      references corsi (idcorso);
 
-alter table tags_post add constraint FKtag_pos_1
-     foreign key (idpost)
-     references posts (idpost);
-
-alter table tags_post add constraint FKtag_tag
-     foreign key (tipo, idcorso)
-     references tags (tipo, idcorso);
-
 alter table utenti add constraint FKappartenenza
      foreign key (idfacolta)
      references facolta (idfacolta);
 
-COMMIT;
+alter table utenti add constraint FKR_1_FK
+     foreign key (identita)
+     references entita (identita);
+
+alter table visione_posts add constraint FKvis_fac
+     foreign key (idfacolta)
+     references facolta (idfacolta);
+
+alter table visione_posts add constraint FKvis_pos
+     foreign key (idpost)
+     references posts (idpost);
+
+
+-- Index Section
+-- _____________
