5c2f7f634df6606738df15c9d4a9ed183432a9cd
Tommaso Goni <tommasogo99@gmail.com>
2026-01-03 15:32:23 +0100

    Add Simple dependency injection container

---
+++ apache/www/app/Core/Container.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Unibostu\Core;
+
+/**
+ * Simple dependency injection container
+ */
+class Container {
+    private array $services = [];
+    private array $instances = [];
+    
+    /**
+     * Register a service with a factory callable
+     *
+     * @param string $name The name of the service
+     * @param callable $factory A callable that returns an instance of the service
+     */
+    public function register(string $name, callable $factory): void {
+        $this->services[$name] = $factory;
+    }
+
+    /**
+     * Get a service instance by name
+     *
+     * @param string $name The name of the service
+     * @return mixed The service instance
+     */ 
+    public function get(string $name) {
+        if (!isset($this->instances[$name])) {
+            if (!isset($this->services[$name])) {
+                throw new \InvalidArgumentException("Service '$name' not found");
+            }
+            $this->instances[$name] = call_user_func($this->services[$name]);
+        }
+        return $this->instances[$name];
+    }
+    
+
+    /**
+     * Check if a service is registered
+     *
+     * @param string $name The name of the service
+     * @return bool True if the service is registered, false otherwise
+     */
+    public function has(string $name): bool {
+        return isset($this->services[$name]);
+    }
+}
