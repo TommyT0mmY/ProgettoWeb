0d6954182b16ed56c31143f78bfbff71aac10703
Tommaso Goni <tommasogo99@gmail.com>
2026-01-03 15:37:58 +0100

    Add BaseController with rendering capabilities

---
+++ apache/www/app/Controller/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Unibostu\Controller;
+
+use Unibostu\Core\RenderingEngine; 
+use Unibostu\Core\Container;
+use Unibostu\Core\SessionManager;
+use Unibostu\Core\Http\Response;
+
+abstract class BaseController {
+    protected RenderingEngine $view;
+
+    public function __construct(Container $container) {
+        $this->view = $container->get(RenderingEngine::class);
+    }
+
+    protected function render(string $template, array $data = []): Response {
+        $content = $this->view->render($template, $data);
+        return new Response($content); 
+    }
+}
