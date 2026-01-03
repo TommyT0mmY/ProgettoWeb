637adf95e48d310064218acf9dd2799e3492cf80
Luigi Linari <luigi.linari@studio.unibo.it>
2026-01-03 02:21:38 +0100

    added indexes and fixed syntax mistakes

--- db/database.sql
+++ db/database.sql
@@ -14,14 +14,18 @@ create table amministratori (
      constraint IDamministratori primary key (idamministratore)
 );
 
-create table castegorie_posts (
+create table categorie_posts (
      idpost int not null,
-     idcategoria varchar(30) not null,
-     constraint IDcastegorie_post primary key (idcategoria, idpost));
+     idcategoria int not null,
+     constraint IDcategorie_post primary key (idcategoria, idpost)
+);
 
 create table categorie (
-     idcategoria varchar(30) not null,
-     constraint IDcategorie primary key (idcategoria));
+     idcategoria int not null auto_increment,
+     nome_categoria varchar(60) not null,
+     constraint IDcategorie primary key (idcategoria),
+     unique (nome_categoria)
+);
 
 create table commenti (
      idcommento int not null auto_increment,
@@ -29,10 +33,10 @@ create table commenti (
      testo text not null,
      data_creazione date not null,
      cancellato boolean not null default false,
-     idutente int not null,
-     idpost_genitore int not null,
+     idutente varchar(60) not null,
      idcommento_genitore int,
-     constraint IDcommenti primary key (idcommento, idpost));
+     constraint IDcommenti primary key (idcommento)
+);
 
 create table corsi (
      idcorso int not null auto_increment,
@@ -43,28 +47,32 @@ create table corsi (
 create table facolta (
      idfacolta int not null auto_increment,
      nome_facolta varchar(60) not null,
-     constraint IDcorsi_di_laurea primary key (idfacolta));
+     constraint IDfacolta primary key (idfacolta));
 
 create table post_tags (
      idpost int not null,
      idcorso int not null,
-     tipo varchar(30) not null,
-     constraint IDpost_tags primary key (idpost, idcorso, tipo));
+     idtag int not null,
+     constraint IDpost_tags primary key (idpost, idcorso, idtag));
 
 create table posts (
      idpost int not null auto_increment,
      titolo varchar(100) not null,
      descrizione text not null,
-     percorso_allegato varchar(255) null,
+     percorso_allegato varchar(255) default null,
      data_creazione date not null,
-     idutente int not null,
-     idcorso int,
-     constraint IDposts primary key (idpost));
+     idutente varchar(60) not null,
+     idcorso int not null,
+     constraint IDposts primary key (idpost)
+);
 
 create table tags (
+     idtag int not null auto_increment,
      tipo varchar(30) not null,
      idcorso int not null,
-     constraint IDtags primary key (idcorso, tipo));
+     constraint IDtags primary key (idtag, idcorso),
+     constraint UDX_tipo_corso unique (tipo, idcorso)
+);
 
 create table utenti (
      idutente varchar(60) not null,
@@ -78,7 +86,7 @@ create table utenti (
 
 create table likes (
      idpost int not null,
-     idutente int not null,
+     idutente varchar(60) not null,
      is_like boolean not null,
      constraint IDlikes primary key (idpost, idutente)
 );
@@ -86,25 +94,30 @@ create table likes (
 -- Constraints Section
 -- ___________________ 
 
-alter table castegorie_posts add constraint FKcas_cat
+alter table categorie_posts add constraint FKcas_cat
      foreign key (idcategoria)
-     references categorie (idcategoria);
+     references categorie (idcategoria)
+     on delete cascade;
 
-alter table castegorie_posts add constraint FKcas_pos
+alter table categorie_posts add constraint FKcas_pos
      foreign key (idpost)
-     references posts (idpost);
+     references posts (idpost)
+     on delete cascade;
 
 alter table commenti add constraint FKscrittura
      foreign key (idutente)
-     references utenti (idutente);
+     references utenti (idutente)
+     on delete cascade;
 
 alter table commenti add constraint FKcommenti_posts
      foreign key (idpost)
-     references posts (idpost);
+     references posts (idpost)
+     on delete cascade;
 
 alter table commenti add constraint FKrisposte
-     foreign key (idcommento_genitore, idpost_genitore)
-     references commenti (idcommento, idpost);
+     foreign key (idcommento_genitore)
+     references commenti (idcommento)
+     on delete cascade;
 
 alter table corsi add constraint FKcomposizione
      foreign key (idfacolta)
@@ -112,20 +125,24 @@ alter table corsi add constraint FKcomposizione
      on delete cascade;
 
 alter table post_tags add constraint FKpos_tag
-     foreign key (idcorso, tipo)
-     references tags (idcorso, tipo);
+     foreign key (idcorso, idtag)
+     references tags (idcorso, idtag)
+     on delete cascade;
 
 alter table post_tags add constraint FKpos_pos
      foreign key (idpost)
-     references posts (idpost);
+     references posts (idpost)
+     on delete cascade;
 
 alter table posts add constraint FKcreazione
      foreign key (idutente)
-     references utenti (idutente);
+     references utenti (idutente)
+     on delete cascade;
 
 alter table posts add constraint FKcorso_posts
      foreign key (idcorso)
-     references corsi (idcorso);
+     references corsi (idcorso)
+     on delete cascade;
 
 alter table tags add constraint FKcorsi_tags
      foreign key (idcorso)
@@ -137,8 +154,20 @@ alter table utenti add constraint FKappartenenza
      references facolta (idfacolta)
      on delete set null;
 
+alter table likes add constraint FKlike_utente
+     foreign key (idutente)
+     references utenti (idutente)
+     on delete cascade;
+
+alter table likes add constraint FKlike_post
+     foreign key (idpost)
+     references posts (idpost)
+     on delete cascade;
 
 -- Index Section
 -- _____________
 
 create index IDX_likes_post on likes (idpost);
+create index IDX_posts_corso on posts (idcorso);
+create index IDX_commenti_post on commenti (idpost);
+create index IDX_post_utente on posts (idutente);
