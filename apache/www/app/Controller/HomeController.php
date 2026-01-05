824db1d7a8dca1cdfe364ac81fb36f4691ff29e5
Tommaso Goni <tommasogo99@gmail.com>
2026-01-05 07:33:02 +0100

    Rendering / to the "home" view

--- apache/www/app/Controller/HomeController.php
+++ apache/www/app/Controller/HomeController.php
@@ -3,21 +3,14 @@ declare(strict_types=1);
 
 namespace Unibostu\Controller;
 
-use Unibostu\Core\CsrfProtection;
 use Unibostu\Core\Http\Request;
 use Unibostu\Core\Http\Response;
+use Unibostu\Model\DTO\PostDTO;
 
 class HomeController extends BaseController {
+    
     public function index(): Response {
-        $csrfman = $this->container->get(CsrfProtection::class);
-        $csrftoken = $csrfman->generateToken('home_page_form');
-        $validity = $csrfman->validateToken($csrftoken, 'home_page_form') ? 'valid' : 'invalid';
-        return new Response("Welcome to the Home Page! $csrftoken | $validity");
-    }
-
-    public function userid(array $params, Request $request): Response {
-        $userId = $params['id'] ?? 'unknown';
-        return new Response("User ID: " . htmlspecialchars($userId) . "Request Method: " . htmlspecialchars($request->getMethod()));
+        return $this->render("home", []);
     }
 }
 
