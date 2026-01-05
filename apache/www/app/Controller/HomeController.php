e627d7017b60759f9ed797bdc1dc3ef1004626d2
Tommaso Goni <tommasogo99@gmail.com>
2026-01-05 21:38:30 +0100

    Using the new routing syntax

--- apache/www/app/Controller/HomeController.php
+++ apache/www/app/Controller/HomeController.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace Unibostu\Controller;
 
-use Unibostu\Core\Http\Request;
 use Unibostu\Core\Http\Response;
-use Unibostu\Model\DTO\PostDTO;
+use Unibostu\Core\router\routes\Get;
 
 class HomeController extends BaseController {
     
+    #[Get('/')]
     public function index(): Response {
         return $this->render("home", []);
     }
--- apache/www/app/Controller/PostController.php
+++ apache/www/app/Controller/PostController.php
@@ -5,38 +5,47 @@ namespace Unibostu\Controller;
 
 use Unibostu\Core\Http\Request;
 use Unibostu\Core\Http\Response;
+use Unibostu\Core\router\routes\Delete;
+use Unibostu\Core\router\routes\Get;
+use Unibostu\Core\router\routes\Post;
 
 class PostController extends BaseController {
-
+    #[Post("/api/posts/create")]
     public function createPost(array $params, Request $request): Response {
         return new Response();
     }
 
+    #[Post("/api/posts/search")]
     public function searchPost(array $params, Request $request): Response {
         return new Response();
     }
 
+    #[Get("/api/posts/:postid/comments")]
     public function showComments(array $params, Request $request): Response {
         $postId = $params['postid'] ?? null;
         return new Response();
     }
 
+    #[Post("/api/posts/:postid/comments")]
     public function addComment(array $params, Request $request): Response {
         $postId = $params['postid'] ?? null;
         return new Response();
     }
 
+    #[Delete("/api/posts/:postid")]
     public function deletePost(array $params, Request $request): Response {
         $postId = $params['postid'] ?? null;
         return new Response();
     }
 
+    #[Delete("/api/posts/:postid/comments/:commentid")]
     public function deleteComment(array $params, Request $request): Response {
         $postId = $params['postid'] ?? null;
         $commentId = $params['commentid'] ?? null;
         return new Response();
     }
 
+    #[Post("/api/posts/:postid/like")]
     public function likePost(array $params, Request $request): Response {
         $postId = $params['postid'] ?? null;
         return new Response();
