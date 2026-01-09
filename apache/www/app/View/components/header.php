a69cff363312ec7bf5ff8be2600bd1101b72f8c8
Aya Chbraoui <aya.chbraoui@studio.unibo.it>
2026-01-09 08:58:35 +0100

    Add header component with logo and buttons

---
+++ apache/www/app/View/components/header.php
@@ -0,0 +1,9 @@
+
+<header>
+    <button id="open-sidebar-button"><img class="menu-container" src="./img/menu.svg" alt="menu" /></button>
+    <div class="title_with_logo">
+        <img src="img/logo.png" alt="logo unibostu"/>          
+        <h1> UniboStu </h1>
+    </div>
+    <a href="profile.html"><img class="profile-container" src="./img/user-profile.svg" alt="user-profile" /></a>    
+ </header>
