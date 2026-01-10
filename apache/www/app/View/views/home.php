0e1e659025b7c990a1584b6e8e6f0bdfcf8932cf
Aya Chbraoui <aya.chbraoui@studio.unibo.it>
2026-01-10 06:53:23 +0100

    changed home.php

--- apache/www/app/View/views/home.php
+++ apache/www/app/View/views/home.php
@@ -1,9 +1,11 @@
 <?php 
 /** @var \Unibostu\Dto\PostDto[] $posts */
 
-$this->extend('main-layout', ['title' => 'Unibostu - Homepage']); 
+$this->extend('main-layout', ['title' => 'Unibostu - Homepage'
+]); 
 ?>
-<div class="posts-container">
+
+<div class="post_container">
 <?php foreach ($posts ?? [] as $post): ?>
     <?= $this->component('post', ['post' => $post]) ?>
 <?php endforeach; ?>
