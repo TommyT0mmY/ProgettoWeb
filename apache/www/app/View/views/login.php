d9e6fe99882c7819f0856d9913e5f3dad34017f2
Tommaso Goni <tommasogo99@gmail.com>
2026-01-15 11:48:30 +0100

    Basic login and register forms

--- apache/www/app/View/views/login.php
+++ apache/www/app/View/views/login.php
@@ -3,26 +3,28 @@
 $this->extend('main-layout', [
     'title' => 'Login Unibostu',
     'additionalHeadCode' => [
-        '<script src="js/login.js" defer></script>',
+        '<script type="module" src="js/login.js"></script>',
     ],
 ]);
 ['csrfKey' => $csrfKey, 'csrfToken' => $csrfToken] = $this->generateCsrfPair(true);
 ?>
-<form id="login-form" method="post">
+
+<form id="login-form" method="post" novalidate>
     <fieldset>
         <legend>Login to Unibostu</legend>
+        <output class="form-error-message" for="username password" role="alert"></output>
         <label for="username">Username:</label>
         <span>
-            <input type="text" name="username" id="username" required aria-required="true">
-            <span class="field-error-message" id="username-error" aria-live="polite"></span>
+            <input type="text" name="username" id="username" aria-describedby="username-error" required>
+            <output class="field-error-message" id="username-error" for="username"></output>
         </span>
         <label for="password">Password:</label>
         <span>
-            <input type="password" name="password" id="password" required aria-required="true">
-            <span class="field-error-message" id="password-error" aria-live="polite"></span>
+            <input type="password" name="password" id="password" aria-describedby="password-error" minlength="6" required>
+            <output class="field-error-message" id="password-error" for="password"></output>
         </span>
-        <input type="hidden" name="csrf-token" id="csrf-token" value="<?php echo $csrfToken; ?>">
-        <input type="hidden" name="csrf-key" id="csrf-key" value="<?php echo $csrfKey; ?>">
+        <input type="hidden" name="csrf-token" id="csrf-token" value="<?= $csrfToken; ?>">
+        <input type="hidden" name="csrf-key" id="csrf-key" value="<?= $csrfKey; ?>">
         <button type="submit" id="login-form_submit">Login</button>
     </fieldset>
 </form>
--- apache/www/app/View/views/register.php
+++ apache/www/app/View/views/register.php
@@ -1,4 +1,55 @@
 <?php 
-$this->extend('main-layout', ['title' => 'Register Unibostu']); 
-?>
+/**
+ * @var \Unibostu\Core\RenderingEngine $this
+ * @var FacultyDTO[] $faculties
+ */
 
+use Unibostu\Model\DTO\FacultyDTO;
+
+$this->extend('main-layout', [
+    'title' => 'Register Unibostu',
+    'additionalHeadCode' => [
+        '<script type="module" src="js/register.js"></script>',
+    ],
+]);
+['csrfKey' => $csrfKey, 'csrfToken' => $csrfToken] = $this->generateCsrfPair(true);
+?>
+<form id="register-form" method="post" novalidate>
+    <fieldset>
+        <legend>Register to Unibostu</legend>
+        <output class="form-error-message" for="username firstname lastname password facultyid" role="alert"></output>
+        <label for="username">Username:</label>
+        <span>
+            <input type="text" name="username" id="username" aria-describedby="username-error" required>
+            <output class="field-error-message" id="username-error" for="username"></output>
+        </span>
+        <label for="firstname">First Name:</label>
+        <span>
+            <input type="text" name="firstname" id="firstname" aria-describedby="firstname-error" required>
+            <output class="field-error-message" id="firstname-error" for="firstname"></output>
+        </span>
+        <label for="lastname">Last Name:</label>
+        <span>
+            <input type="text" name="lastname" id="lastname" aria-describedby="lastname-error" required>
+            <output class="field-error-message" id="lastname-error" for="lastname"></output>
+        </span>
+        <label for="password">Password:</label>
+        <span>
+            <input type="password" name="password" id="password" aria-describedby="password-error" minlength="6" required>
+            <output class="field-error-message" id="password-error" for="password"></output>
+        </span>
+        <label for="facultyid">Faculty ID:</label>
+        <span>
+            <select name="facultyid" id="facultyid" aria-describedby="facultyid-error" required>
+                <option value="" disabled selected>Select your faculty</option>
+                <?php foreach ($faculties as $faculty): ?>
+                <option value="<?= htmlspecialchars($faculty->facultyId); ?>"><?= htmlspecialchars($faculty->facultyName); ?></option>
+                <?php endforeach; ?>
+            </select>
+            <output class="field-error-message" id="facultyid-error" for="facultyid"></output>
+        </span>
+        <input type="hidden" name="csrf-token" id="csrf-token" value="<?= $csrfToken; ?>">
+        <input type="hidden" name="csrf-key" id="csrf-key" value="<?= $csrfKey; ?>">
+        <button type="submit" id="register-form_submit" disabled>Register</button>
+    </fieldset>
+</form>
