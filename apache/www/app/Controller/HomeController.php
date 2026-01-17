e89e9595796ad23657903c6ae3121c701e5dd776
Luigi Linari <luigi.linari@studio.unibo.it>
2026-01-17 19:24:46 +0100

    homecontroller updated for new features but still in testing

--- apache/www/app/Controller/HomeController.php
+++ apache/www/app/Controller/HomeController.php
@@ -3,15 +3,21 @@ declare(strict_types=1);
 
 namespace Unibostu\Controller;
 
+use Exception;
 use Unibostu\Core\Http\Response;
 use Unibostu\Core\Http\Request;
 use Unibostu\Model\DTO\PostQuery;
 use Unibostu\Core\router\routes\Get;
 use Unibostu\Model\Service\PostService;
+use Unibostu\Core\security\Auth;
+use Unibostu\Model\Service\CourseService;
+use Unibostu\Model\Service\CategoryService;
 
 class HomeController extends BaseController {
     private $postService;
-    
+    private $courseService;
+    private $categoryService;
+
     #[Get('/')]
     public function index(): Response {
         return $this->render("home", []);
@@ -19,15 +25,29 @@ class HomeController extends BaseController {
 
     #[Get('/homepage')]
     public function getHomepagePosts(array $params, Request $request): Response {
-        $postQuery = PostQuery::create()->forAdmin(true);
-/*             ->forUser("alice.testa")
-            ->inCategory($request->get('categoryId'))
-            ->sortedBy($request->get('sortOrder'))
-            ->afterPost((int)$request->get('lastPostId'))
-            ->withLimit((int)$request->get('limit'));
- */
+        $postQuery = null; 
+        $userId = "mrossi"; //$this->getContainer()->get(Auth::class)->getAuthenticatedUserId();                             
+        if ($this->getContainer()->get(Auth::class)->isAuthenticatedAsAdmin()) {
+            $postQuery = PostQuery::create()
+                ->forAdmin(true);                                             //|| true per testing poi lo tolgo
+        } else if ($this->getContainer()->get(Auth::class)->isAuthenticatedAsUser() || true) {
+            $postQuery = PostQuery::create()
+                ->forUser($userId)
+                ->inCategory($request->get('categoryId'))
+                ->withTags($request->get('tags'))
+                ->sortedBy($request->get('sortOrder'));
+        } else {
+            throw new Exception('You are not authenticated');
+        }
+
         $this->postService = new PostService();
-        return $this->render("home", ["posts" => $this->postService->getPosts($postQuery)]);
+        $this->courseService = new CourseService();
+        $this->categoryService = new CategoryService();
+        return $this->render("home", [
+            "posts" => $this->postService->getPosts($postQuery),
+            "courses" => $this->courseService->getCoursesByUser($userId),
+            "categories" => $this->categoryService->getAllCategories()
+        ]);
     }
 }
 
