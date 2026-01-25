2fd064c5053a7167f4c8898fedb1380c2941158d
Tommaso Goni <tommasogo99@gmail.com>
2026-01-25 18:35:40 +0100

    ValidationMiddleware now validates (checks for not null) request body fields putting them in Request Attribute FIELDS

--- apache/www/app/Core/router/middleware/ValidationMiddleware.php
+++ apache/www/app/Core/router/middleware/ValidationMiddleware.php
@@ -6,13 +6,22 @@ namespace Unibostu\Core\router\middleware;
 use Attribute;
 use Unibostu\Core\exceptions\DomainErrorCode;
 use Unibostu\Core\exceptions\ValidationException;
+use Unibostu\Core\exceptions\ValidationExceptionBuilder;
 use Unibostu\Core\Http\Request;
+use Unibostu\Core\Http\RequestAttribute;
 use Unibostu\Core\Http\RequestHandlerInterface;
 use Unibostu\Core\Http\Response;
 use Unibostu\Core\security\CsrfProtection;
 
 #[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
 class ValidationMiddleware extends AbstractMiddleware {
+
+    public function __construct(
+        /** @var array<string, ValidationErrorCode> */
+        private array $mandatoryBodyFields = [],
+        /** @var string[] */
+        private array $optionalBodyFields = []
+    ) {}
 
     public function process(Request $request, RequestHandlerInterface $handler): Response {
         // Csrf protection
@@ -21,16 +30,35 @@ class ValidationMiddleware extends AbstractMiddleware {
             return Response::create()->json([
                 "success" => false,
                 "errors" => [DomainErrorCode::GENERIC_ERROR->name]
-            ]);
+            ], 403);
         }
         // Catching validation exceptions
         try {
+            // Capturing mandatory and optional body fields
+            /** 
+             * @var array<string, mixed> $capturedFields 
+             * @var ValidationExceptionBuilder $validationExceptionBuilder 
+             */
+            $capturedFields = [];
+            $validExcBuilder = ValidationException::build();
+            foreach ($this->mandatoryBodyFields as $fieldName => $errorCode) {
+                $fieldValue = $request->post($fieldName);
+                if ($fieldValue === null) {
+                    $validExcBuilder->addError($errorCode);
+                }
+                $capturedFields[$fieldName] = $fieldValue;
+            }
+            foreach ($this->optionalBodyFields as $fieldName) {
+                $capturedFields[$fieldName] = $request->post($fieldName);
+            }
+            $validExcBuilder->throwIfAny();
+            $request = $request->withAttribute(RequestAttribute::FIELDS, $capturedFields);
             return $handler->handle($request);
         } catch (ValidationException $e) {
             return Response::create()->json([
                 "success" => false,
                 "errors" => $e->getErrorCodes()
-            ]);
+            ], 400);
         }
     }
 }
