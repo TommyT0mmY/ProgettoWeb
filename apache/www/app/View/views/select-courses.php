ede73fd131ffdbff9e9864657618b39ca3fe0dbd
Tommaso Goni <tommasogo99@gmail.com>
2026-01-28 15:23:59 +0100

    Initial implementation of user course selector

--- apache/www/app/View/views/select-courses.php
+++ apache/www/app/View/views/select-courses.php
@@ -1,36 +1,49 @@
 <?php 
 /** 
- * @var \Unibostu\Core\RenderingEngine $this 
- * @var array $subscribedCourses 
- * @var array<Unibostu\Model\DTO\FacultyDTO> $faculties
+ * @var \Unibostu\Core\RenderingEngine $this
+ * @var \Unibostu\Model\DTO\FacultyDTO[] $faculties
+ * @var int|null $userFacultyId
  */
 $this->extend('loggedout-layout', [
     'title' => 'Select Courses - Unibostu',
     'additionalHeadCode' => [
         '<script type="module" src="/js/select-courses.js"></script>',
         '<link rel="stylesheet" href="/css/fsform.css" />',
+        '<link rel="stylesheet" href="/css/select-courses.css" />',
     ],
 ]);
 ['csrfKey' => $csrfKey, 'csrfToken' => $csrfToken] = $this->generateCsrfPair(true);
+
 ?>
 
 <form class="fullscreen-form" id="select-courses-form" method="post" novalidate>
     <fieldset>
-        <legend>Legend</legend>
-        <output class="form-error-message" for="username password" role="alert"></output>
+        <legend>Manage Courses</legend>
+        <output class="form-error-message" for="courses" role="alert"></output>
+
+        <!-- Faculty Selector -->
         <div class="field-holder">
-            <input type="text" name="username" id="username" aria-describedby="username-error" required />
-            <label for="username">Username</label>
-            <output class="field-error-message" id="username-error" for="username"></output>
+            <select id="faculty-selector" aria-describedby="faculty-selector-error">
+                <?php foreach ($faculties as $faculty): ?>
+                <option value="<?= htmlspecialchars($faculty->facultyId); ?>"<?= $userFacultyId === $faculty->facultyId ? ' selected' : ''; ?>>
+                    <?= htmlspecialchars($faculty->facultyName); ?>
+                </option>
+                <?php endforeach; ?>
+            </select>
+            <label for="faculty-selector">Faculty</label>
+            <output class="field-error-message" id="faculty-selector-error" for="faculty-selector"></output>
         </div>
-        <div class="field-holder">
-            <input type="password" name="password" id="password" aria-describedby="password-error" minlength="6" required />
-            <label for="password">Password</label>
-            <output class="field-error-message" id="password-error" for="password"></output>
+
+        <!-- Courses Container -->
+        <div id="courses-container" aria-label="Courses list">
         </div>
+
         <input type="hidden" name="csrf-token" id="csrf-token" value="<?= $csrfToken; ?>" />
         <input type="hidden" name="csrf-key" id="csrf-key" value="<?= $csrfKey; ?>" />
-        <button type="submit" id="select-courses-submit" disabled>Save Preferences</button>
+        <div class="controls-container">
+            <button type="button" onclick="history.back()">Back</button>
+            <button type="submit" id="select-courses-submit" disabled>Save</button>
+        </div>
     </fieldset>
 </form>
 
