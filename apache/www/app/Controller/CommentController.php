4adc47c036194965e35af61037557e2b9607037a
Luigi Linari <luigi.linari@studio.unibo.it>
2026-01-28 19:03:15 +0100

    comments now have their own controller

---
+++ apache/www/app/Controller/CommentController.php
@@ -0,0 +1,131 @@
+<?php
+declare(strict_types=1);
+
+namespace Unibostu\Controller;
+
+use Unibostu\Core\Container;
+use Unibostu\Core\exceptions\ValidationErrorCode;
+use Unibostu\Core\exceptions\ValidationException;
+use Unibostu\Core\Http\Request;
+use Unibostu\Core\Http\RequestAttribute;
+use Unibostu\Core\Http\Response;
+use Unibostu\Core\router\middleware\AuthMiddleware;
+use Unibostu\Core\router\middleware\ValidationMiddleware;
+use Unibostu\Core\router\routes\Delete;
+use Unibostu\Core\router\routes\Get;
+use Unibostu\Core\router\routes\Post;
+use Unibostu\Core\security\Role;
+use Unibostu\Model\DTO\CreateCommentDTO;
+use Unibostu\Model\DTO\CreatePostDTO;
+use Unibostu\Model\Service\PostService;
+use Unibostu\Model\Service\CommentService;
+use Unibostu\Model\Service\CourseService;
+use Unibostu\Model\Service\CategoryService;
+use Unibostu\Model\Service\TagService;
+use Unibostu\Model\Service\UserService;
+
+
+class CommentController extends BaseController {
+    private $postService;
+    private $commentService;
+    private $courseService;
+    private $categoryService;
+    private $tagService;
+    private $userService;
+
+    public function __construct(Container $container) {
+        parent::__construct($container);
+        $this->postService = new PostService();
+        $this->commentService = new CommentService();
+        $this->courseService = new CourseService();
+        $this->categoryService = new CategoryService();
+        $this->tagService = new TagService();
+        $this->userService = new UserService();
+    }
+
+    #[Post("/api/posts/:postid/comments")]
+    #[AuthMiddleware(Role::USER)]
+    #[ValidationMiddleware([
+        "text" => ValidationErrorCode::COMMENT_TEXT_REQUIRED
+    ], ["parentCommentId"])]
+    public function addComment(Request $request): Response {
+        $pathVars = $request->getAttribute(RequestAttribute::PATH_VARIABLES);
+        $postId = $pathVars['postid'] ?? null;
+        if ($postId === null) {
+            throw new ValidationException(errors: [ValidationErrorCode::POST_ID_REQUIRED]);
+        }
+        $userId = $request->getAttribute(RequestAttribute::ROLE_ID);
+        [ "text" => $text, "parentCommentId" => $parentCommentId ] = $request->getAttribute(RequestAttribute::FIELDS);
+        $parentCommentId = isset($parentCommentId) ? (int)$parentCommentId : null;
+        $commentWithAuthor = $this->commentService->createComment(new CreateCommentDTO(
+            postId: (int)$postId,
+            userId: $userId,
+            text: $text,
+            parentCommentId: $parentCommentId
+        ));
+        return Response::create()->json([
+            'success' => true,
+            'data' => [
+                'commentId' => $commentWithAuthor->commentId,
+                'postId' => $commentWithAuthor->postId,
+                'text' => $commentWithAuthor->text,
+                'createdAt' => $commentWithAuthor->createdAt,
+                'deleted' => $commentWithAuthor->deleted,
+                'parentCommentId' => $commentWithAuthor->parentCommentId,
+                'author' => [
+                    'userId' => $commentWithAuthor->author->userId,
+                    'firstName' => $commentWithAuthor->author->firstName,
+                    'lastName' => $commentWithAuthor->author->lastName,
+                ]
+            ]
+        ], 201);
+    }
+
+    #[Get("/api/posts/:postid/comments")]
+    #[AuthMiddleware(Role::USER)]
+    public function showComments(Request $request): Response {
+        $pathVars = $request->getAttribute(RequestAttribute::PATH_VARIABLES);
+        $postId = $pathVars['postid'] ?? null;
+        $comments = $this->commentService->getCommentsByPostId((int)$postId);
+        $commentsArray = array_map(function($commentDTO) {
+                return [
+                    'commentId' => $commentDTO->commentId,
+                    'postId' => $commentDTO->postId,
+                    'text' => $commentDTO->text,
+                    'createdAt' => $commentDTO->createdAt,
+                    'deleted' => $commentDTO->deleted,
+                    'parentCommentId' => $commentDTO->parentCommentId,
+                    'author' => [
+                        'userId' => $commentDTO->author->userId,
+                        'firstName' => $commentDTO->author->firstName,
+                        'lastName' => $commentDTO->author->lastName,
+                    ]
+                ];
+            }, $comments);
+        return Response::create()->json([
+            'success' => true,
+            'data' => $commentsArray
+        ]);
+    }
+    
+    #[Delete("/api/posts/:postid/comments/:commentid")]
+    #[AuthMiddleware(Role::USER)]
+    #[ValidationMiddleware()]
+    public function deleteComment(Request $request): Response {
+        $pathVars = $request->getAttribute(RequestAttribute::PATH_VARIABLES);
+        $postId = $pathVars['postid'] ?? null;
+        $commentId = $pathVars['commentid'] ?? null;
+        if ($postId === null) {
+            throw new ValidationException(errors: [ValidationErrorCode::POST_ID_REQUIRED]);
+        }
+        if ($commentId === null) {
+            throw new ValidationException(errors: [ValidationErrorCode::COMMENT_ID_REQUIRED]);
+        }
+        $userId = $request->getAttribute(RequestAttribute::ROLE_ID);
+        $this->commentService->deleteComment((int)$commentId, (int)$postId, $userId);
+        return Response::create()->json([
+            'success' => true,
+            'message' => 'Comment deleted'
+        ]);
+    }
+}
--- apache/www/app/Controller/PostController.php
+++ apache/www/app/Controller/PostController.php
@@ -78,44 +78,6 @@ class PostController extends BaseController {
         ]);
     }
 
-    #[Post("/api/posts/:postid/comments")]
-    #[AuthMiddleware(Role::USER)]
-    #[ValidationMiddleware([
-        "text" => ValidationErrorCode::COMMENT_TEXT_REQUIRED
-    ], ["parentCommentId"])]
-    public function addComment(Request $request): Response {
-        $pathVars = $request->getAttribute(RequestAttribute::PATH_VARIABLES);
-        $postId = $pathVars['postid'] ?? null;
-        if ($postId === null) {
-            throw new ValidationException(errors: [ValidationErrorCode::POST_ID_REQUIRED]);
-        }
-        $userId = $request->getAttribute(RequestAttribute::ROLE_ID);
-        [ "text" => $text, "parentCommentId" => $parentCommentId ] = $request->getAttribute(RequestAttribute::FIELDS);
-        $parentCommentId = isset($parentCommentId) ? (int)$parentCommentId : null;
-        $commentWithAuthor = $this->commentService->createComment(new CreateCommentDTO(
-            postId: (int)$postId,
-            userId: $userId,
-            text: $text,
-            parentCommentId: $parentCommentId
-        ));
-        return Response::create()->json([
-            'success' => true,
-            'data' => [
-                'commentId' => $commentWithAuthor->commentId,
-                'postId' => $commentWithAuthor->postId,
-                'text' => $commentWithAuthor->text,
-                'createdAt' => $commentWithAuthor->createdAt,
-                'deleted' => $commentWithAuthor->deleted,
-                'parentCommentId' => $commentWithAuthor->parentCommentId,
-                'author' => [
-                    'userId' => $commentWithAuthor->author->userId,
-                    'firstName' => $commentWithAuthor->author->firstName,
-                    'lastName' => $commentWithAuthor->author->lastName,
-                ]
-            ]
-        ], 201);
-    }
-
     #[Post("/api/posts/create")]
     #[AuthMiddleware(Role::USER)]
     #[ValidationMiddleware([
@@ -140,7 +102,7 @@ class PostController extends BaseController {
         
         // Handle file upload
         $attachmentPath = null;
-        // TODO
+        // TODO: Implement file upload handling
         
         $createPostDTO = new CreatePostDTO(
             userId: $userId,
@@ -164,33 +126,6 @@ class PostController extends BaseController {
     #[AuthMiddleware(Role::USER)]
     public function searchPost(Request $request): Response {
         return new Response();
-    }
-
-    #[Get("/api/posts/:postid/comments")]
-    #[AuthMiddleware(Role::USER)]
-    public function showComments(Request $request): Response {
-        $pathVars = $request->getAttribute(RequestAttribute::PATH_VARIABLES);
-        $postId = $pathVars['postid'] ?? null;
-        $comments = $this->commentService->getCommentsByPostId((int)$postId);
-        $commentsArray = array_map(function($commentDTO) {
-                return [
-                    'commentId' => $commentDTO->commentId,
-                    'postId' => $commentDTO->postId,
-                    'text' => $commentDTO->text,
-                    'createdAt' => $commentDTO->createdAt,
-                    'deleted' => $commentDTO->deleted,
-                    'parentCommentId' => $commentDTO->parentCommentId,
-                    'author' => [
-                        'userId' => $commentDTO->author->userId,
-                        'firstName' => $commentDTO->author->firstName,
-                        'lastName' => $commentDTO->author->lastName,
-                    ]
-                ];
-            }, $comments);
-        return Response::create()->json([
-            'success' => true,
-            'data' => $commentsArray
-        ]);
     }
 
     #[Delete("/api/posts/:postid")]
@@ -223,27 +158,6 @@ class PostController extends BaseController {
         }
     }
     
-    #[Delete("/api/posts/:postid/comments/:commentid")]
-    #[AuthMiddleware(Role::USER)]
-    #[ValidationMiddleware()]
-    public function deleteComment(Request $request): Response {
-        $pathVars = $request->getAttribute(RequestAttribute::PATH_VARIABLES);
-        $postId = $pathVars['postid'] ?? null;
-        $commentId = $pathVars['commentid'] ?? null;
-        if ($postId === null) {
-            throw new ValidationException(errors: [ValidationErrorCode::POST_ID_REQUIRED]);
-        }
-        if ($commentId === null) {
-            throw new ValidationException(errors: [ValidationErrorCode::COMMENT_ID_REQUIRED]);
-        }
-        $userId = $request->getAttribute(RequestAttribute::ROLE_ID);
-        $this->commentService->deleteComment((int)$commentId, (int)$postId, $userId);
-        return Response::create()->json([
-            'success' => true,
-            'message' => 'Comment deleted'
-        ]);
-    }
-
     #[Post("/api/posts/:postid/like")]
     #[AuthMiddleware(Role::USER)]
     #[ValidationMiddleware()]
